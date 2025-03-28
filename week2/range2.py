a6a9dd9cd2f131c89c16b592af79074b5557604d
Aswini369643 <aswiniikkiri@gmail.com>
2025-03-28 16:19:47 +0530

    Create range2.py

---
+++ week2/range2.py
@@ -0,0 +1,9 @@
+n=int(input("enter a number')
+for 1 in range(2,n):
+    flag=0 
+    for j in range(2,1):
+        if(1%j==0):
+           flag=1
+           break;
+       if(flag==0):
+          print(1)
