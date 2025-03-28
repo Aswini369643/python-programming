b2c51a9e4f74f7478020949b8887eb6fd703bb31
Aswini369643 <aswiniikkiri@gmail.com>
2025-03-28 16:14:20 +0530

    Create prime.py

---
+++ week2/prime.py
@@ -0,0 +1,11 @@
+n=int(input("enter a number"))
+flag=0
+for i in range(2,n):
+    if(n%i==0):
+       print("not a prime number")
+       flag=1
+       break:
+else:
+    if(flag==0):
+      print("is a prime number")
+
