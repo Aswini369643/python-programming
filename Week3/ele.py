47e0ece994ef77c2fcdb42f147e62d72cb51dd01
Aswini369643 <aswiniikkiri@gmail.com>
2025-04-04 15:59:56 +0530

    Create ele.py

---
+++ Week3/ele.py
@@ -0,0 +1,7 @@
+elements=[]
+limit=int(input("enter the elements"))
+for i in range(limit):
+    ele=int(input())
+    element.append(ele)
+for i in range(len(elements)):
+    print(element[i],end='')
